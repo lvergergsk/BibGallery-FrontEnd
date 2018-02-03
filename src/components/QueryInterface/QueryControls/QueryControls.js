067a85cc5d28e3f211a6ec43fc7578fcab88b247
YOULYU ZHANG <lvergergsk@outlook.com>
2018-02-03 18:38:35 -0500

    A very basic version of page layout and project layout. Need to be improved. Routing not added.

---
+++ src/components/QueryInterface/QueryControls/QueryControls.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+import ReactAux from '../../../hoc/ReactAux'
+import QueryControl from './QueryControl/QueryControl'
+
+const QueryControls = (props) => (
+    <ReactAux>
+        <QueryControl/>
+        <QueryControl/>
+        <QueryControl/>
+        <QueryControl/>
+        <QueryControl/>
+        <QueryControl/>
+        <QueryControl/>
+        <QueryControl/>
+    </ReactAux>
+
+)
+
+export default QueryControls
